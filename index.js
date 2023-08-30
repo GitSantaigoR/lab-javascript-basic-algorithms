ede3088732c25c44c82541002e65667c2dc164c3
Santiago Rivera <santiagorockshop1996@hotmail.com>
2023-08-30 17:44:43 -0400

    Lab done

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
-// Iteration 1: Names and Input
+let hacker1 = "Anthony";
+console.log(`The driver's name is ${hacker1}`);
+
+let hacker2 = "Santiago";
+console.log(`The navigator's name is ${hacker2}`);
 
 
-// Iteration 2: Conditionals
+if (hacker1.length > hacker2.length) {
+  console.log(`The driver has the longest name, it has ${hacker1.length} characters.`) ;
+} else if (hacker1.length < hacker2.length) {
+  console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`) ;
+} else {
+  console.log(`Wow, you both have equally long names, ${hacker1.length + hacker2.length} characters!`);
+}
 
 
-// Iteration 3: Loops
+let driverString = " ";
+for (let i=0; i<hacker1.length; i++) {
+  let result = hacker1[i].toUpperCase();
+  driverString += result;
+}
+console.log(driverString);
+
+let navigatorString = " ";
+for (let i=hacker2.length - 1; i>=0; i--) {
+  let result2 = hacker2[i];
+  navigatorString += result2;
+}
+console.log(navigatorString);
+if (driverString < navigatorString) {
+  console.log("The driver's name goes first.");
+} else if (driverString > navigatorString) {
+  console.log("Yo, the navigator goes first, definitely.");
+} else {
+  console.log("What?! You both have the same name?");
+}
